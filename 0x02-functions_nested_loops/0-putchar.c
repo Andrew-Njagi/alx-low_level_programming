a4a3470608d230fd89994f0ad2675175c82d6df1
Andrew-Njagi <andrewnjagigitari@outlook.com>
2022-03-16 16:24:01 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- *main- prints Holberton, followed by a new line
- *Return:always 0 (Success)
+ * main - Prints as a message.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i = 0;
-char msg[] = "_putchar\n";
-char c;
-while (i <= 8)
-{
-c = msg[i];
-_putchar(c);
-i++;
-}
-return (0);
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
